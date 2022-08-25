1d91f69996ea6f6d8f353ec2cacc3b0a660e81bf
Hamza ELBOHDIDI <bohdidi3@gmail.com>
2022-08-25 16:50:43 +0100

    pushing react bar-chart-project to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css';
+import LineChart from './LineChart';
 
 function App() {
+  const[data, setData] = useState([]) 
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+        .then(response => response.json())
+        .then(data => setData(data.data))
+  }, [])
+
+  const width = 800;
+  const height = 600;
+  const padding = 40; 
+  const widthOfBar = width / data.length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <LineChart
+        padding={padding}
+        width={width}
+        height={height}
+        data={data}
+        widthOfBar={widthOfBar}
+        />
     </div>
   );
 }
---
+++ src/LineChart.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react'
+import './LineChart.css'
+import * as d3 from 'd3'
+
+const LineChart = ({data, width, height, widthOfBar, padding}) => {
+    
+    useEffect(() => {
+        createChart();
+    }, [data])
+
+    const createChart = () => {
+        const values = data.map(item => item[1])
+        const dates = data.map(item => new Date(item[0]))
+        
+        const years = data.map(item => {
+            let temp = item[0].substring(5,7)
+            let quarter;
+            if(temp === '01'){
+                quarter = 'Q1' 
+            } else if(temp === '04'){
+                quarter = 'Q2'
+            } else if(temp === '07'){
+                quarter = 'Q3'
+            } else if(temp === '10'){
+                quarter = 'Q4'
+            }
+
+            return item[0].substring(0,4) + ' ' + quarter;
+        })
+
+            
+        const tooltip = d3.select('#visHolder')
+                          .append('div')
+                          .attr('id', "tooltip")
+                          .style('visibility', 'hidden')
+
+        const xScale = d3.scaleLinear()
+                         .domain([0, data.length - 1 ])
+                         .range([padding, width - padding])
+
+        const yScale = d3.scaleLinear()
+                         .domain([0, d3.max(data, item =>{
+                            return item[1]
+                         })])
+                         .range([0, height - (padding * 2)])
+
+        const xScaleAxis = d3.scaleTime()
+                             .domain([d3.min(dates), d3.max(dates)])
+                             .range([padding, width - padding])
+
+        const yScaleAxis = d3.scaleLinear()
+                             .domain([0, d3.max(data, item =>{
+                                return item[1]
+                            })])
+                             .range([height - (padding * 2), 0])   
+
+
+        const xAxis = d3.axisBottom(xScaleAxis)
+        const yAxis = d3.axisLeft(yScaleAxis)
+                             
+        d3.select('svg')
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('height', (item) => yScale(item[1]))
+        .attr('width', (width - (2 * padding)) / data.length) 
+        .attr('x', (item, index) => {
+            return xScale(index)
+        })
+        .attr('y', (item) => {
+            return (height - padding) - yScale(item[1])
+        })
+        .attr('data-date', item => item[0])
+        .attr('data-gdp', item => item[1])
+        .on('mouseover', (e ,item) => {
+            tooltip
+                    .style('visibility', 'visible')
+                    .html(`${years[data.indexOf(item)]}` + ` <br/> $${item[1]} Billion`)
+                    .style('left', (e.pageX - (widthOfBar - 20)) + 'px')
+                    .style('top', (e.pageY - 40) + 'px')
+            })
+            .on('mouseout', () => {
+                tooltip.style('visibility', 'hidden');
+                });
+
+            d3.select('svg').append('g')
+                        .call(xAxis)
+                        .attr('id', 'x-axis')
+                        .attr('transform', 'translate(0,' + (height - padding) + ')')
+            d3.select('svg').append('g')
+                        .call(yAxis)
+                        .attr('id', 'y-axis')
+                        .attr('transform', 'translate(' + padding + ',' + padding + ')')
+            }
+
+    return (
+        <div id='visHolder'>
+            <h1 id='title'>United States GDP</h1>
+            <svg width={width} height={height}></svg>
+        </div>
+    )
+} 
+
+export default LineChart;
